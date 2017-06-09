99c1ec7fa5f4b501317dbac1d8f2e3e4a3ce409d
solpie <solpie.net@gmail.com>
2017-06-09 14:26:09 +0800

    stater

---
+++ src/app.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="app">
+        <el-tabs type="border-card">
+            <el-tab-pane style="width:250px">
+                <span slot="label"><i class="el-icon-edit"></i> Home</span>
+            </el-tab-pane>
+            <el-tab-pane label="page1">
+            </el-tab-pane>
+        </el-tabs>
+    </div>
+</template>
+
+<script>
+import app from './App'
+export default {
+    created() {
+        app.init(this)
+    }
+}
+</script>
+
+<style>
+body {
+    font-family: Helvetica, sans-serif;
+}
+</style>
