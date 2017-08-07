338f0a321bfdba085ee790688d4ad68e77afa563
solpie <solpie.net@gmail.com>
2017-08-07 16:14:53 +0800

    merge rank arr

--- server/ranking/ranking.js
+++ server/ranking/ranking.js
@@ -59,6 +59,9 @@ function downLoadGameData() {
 }
 // downLoadGameData()
 
+//global
+//{player_id:[{oneGameRec}]}
+const playerGameRecMap = {}
 
 function genPlayer(playerData) {
     return {
@@ -66,104 +69,331 @@ function genPlayer(playerData) {
         name: playerData.name,
         gameIdMap: {},
         win: 0,
+        lastRank: 0, //最近排名
         beatPlayerMap: {},
         losePlayerMap: {},
         meetPlayerWinRaitoMap: {}, //交手胜率
         section: 0, //1最高 ~5
-        champion: 0,
+        master: 0, //大师赛次数
+        champion: 0, //冠军次数
         gameCount: 0
     }
 }
 
-function circlePlayer(playerId, playerMap, num) {
-    let recursion = (start, end, playerMap, nodeNum, path = []) => {
-            // if (!path)
-            //     path = []
-            if (!path.length)
-                path.push(start)
-            if (nodeNum > 0) {
-                // console.log('start', start, 'end', end, path, num);
-                for (let pid in playerMap[start].beatPlayerMap) {
-                    if (path.indexOf(pid) > -1) {
-                        // console.log('back', playerMap[pid].name);
-                    } else {
-                        let p = playerMap[pid]
-                        recursion(pid, end, playerMap, nodeNum - 1, path.concat([pid]))
-                    }
-                }
+function findWinPath(start, end, playerMap, nodeNum, path = []) {
+    if (!path.length)
+        path.push(start)
+    if (nodeNum > 0) {
+        // console.log('start', start, 'end', end, path, num);
+        for (let pid in playerMap[start].beatPlayerMap) {
+            if (path.indexOf(pid) > -1) {
+                // console.log('back', playerMap[pid].name);
             } else {
-                // console.log('out start', start, 'end', end, path, num);
-                for (let pid in playerMap[start].beatPlayerMap) {
-                    if (pid == end) {
-                        path.push(pid)
-                        let pathStr = ""
-                        let lastPid = ""
-                        for (let p2 of path) {
-                            if (lastPid)
-                                pathStr += ' ' + playerMap[lastPid].beatPlayerMap[p2] + 'x'
-                            lastPid = p2
-                            pathStr += "->" + playerMap[p2].name
-                                //+ '[' + p2 + ']'
-                        }
-                        console.log('path', pathStr);
-                    }
-                }
+                let p = playerMap[pid]
+                return findWinPath(pid, end, playerMap, nodeNum - 1, path.concat([pid]))
             }
         }
-        //relationWin
-        //nodeNum 越少关系越强，nodeNum=1为直接胜负参考
-        //击败次数越多 关系越强
-        //赢球百分比 winScore-opScore/winScore
-        //车轮战 1或0.5
-        //大师 1 0.333 0.667
-        //决赛 1 0.2 0.4 0.6 0.8
-        //relationWin 车轮一场+1*赢球百分比
-    recursion('4', '4', playerMap, 2)
-        // recursion('4', '4', playerMap, 1)
+    } else {
+        // console.log('out start', start, 'end', end, path, num);
+        for (let pid in playerMap[start].beatPlayerMap) {
+            if (pid == end) {
+                path.push(pid)
+                let pathStr = ""
+                let lastPid = ""
+                for (let p2 of path) {
+                    if (lastPid)
+                        pathStr += ' ' + Math.floor(playerMap[lastPid].beatPlayerMap[p2] * 100) + '%'
+                    lastPid = p2
+                    pathStr += "->" + playerMap[p2].name
+                        //+ '[' + p2 + ']'
+                }
+                console.log('path', pathStr);
+                return pathStr
+            }
+        }
+    }
+}
+
+function topRelationWin() {
+    //赢过最高排位的
+    //输过最高排位几分
+}
+
+function isAwinB(playerA, playerB, playerMap) {
+    let nodeNum = 0
+    while (nodeNum < 3) {
+        if (findWinPath(playerA.player_id, playerB.player_id, playerMap, 0))
+            return true
+        nodeNum++
+    }
+    return false
+        // if (findWinPath(pidA, pidB, playerMap, 0))
+        // {
+        //     return true
+        // }
+        // else if(findWinPath(pidA,pidB,playerMap))
+}
+
+function circlePlayer(playerId, playerMap, num) {
+    // let recursion = (start, end, playerMap, nodeNum, path = []) => {
+
+    //     }
+    //relationWin关系胜场 
+    //nodeNum 越少关系越强，nodeNum=1为直接胜负参考
+    //击败次数越多 关系越强
+    // recursion('4', '4', playerMap, 1)
+    let pidArr = []
+    for (let pid in playerMap) {
+        pidArr.push(pid)
+    }
+    let sortBy = () => {
+            field = 'beatPlayerMap'
+            return function(a, b) {
+                if (playerMap[a][field][b] && playerMap[b][field][b]) {
+                    if (playerMap[a][field][b] > playerMap[b][field][a])
+                        return 1
+                    else
+                        return -1
+                }
+                return 0;
+            };
+        }
+        //
+    pidArr = pidArr.sort(sortBy)
+    let playerArr = []
+    for (let pid of pidArr) {
+        playerArr.push(playerMap[pid])
+    }
+    console.log('sort', playerArr);
+
+    //winPoint 胜点，当球员A在决赛中击败对手B的时候，可以认为A比B强
+    /**
+     *relationWin关系胜场，目的,计算A比B赢多少的问题。
+            1.有直接交手 A->B 
+     *      一般比赛A赢B我们就算A赢了B一个胜场。其中忽略了比分
+     *      5-0赢和5-4赢对于AB之间的实力差距就无法用只计算一场胜负区分
+     *      因此结合比分scoreRatio以及几种不同比赛的权重winPoint来计算。
+     *      2.交手环 A->B->C->.....->A
+     *      3.无交手 A->B->C->D->...
+     *
+     *nodeNum[nn]  交手权重 
+     *scoreRatio[sr] 赢球百分比 (winScore-opScore)/winScore
+     *winPoint[wp] 胜点，当球员A在决赛中击败对手B的时候，可以认为A比B强
+     *         车轮：0.5   随机性强，可认为双方五五开
+     *         大师：0.7   七成实力
+     *         决赛：1     绝对实力
+     *
+     *车轮战 1或0.5
+     *大师 1 0.333 0.667
+     *决赛 1 0.2 0.4 0.6 0.8
+     *relationWin
+     *   车轮一场 0.25~0.5
+     *      大师  0.42~0.7
+     *      决赛  0.8~1
+     *
+     * 活跃
+     */
+    //relationWin 车轮一场 +0.5*wp
+    //relationWin 大师 +0.7*wp
+    //relationWin final +1*wp
+    findWinPath(playerId, playerId, playerMap, 1)
+}
+
+function countMap(map) {
+    return Object.keys(map).length
 }
 
 function sumPlayer(playerMap) {
-    let playerArrAnimal = []
+    let playerArrRankBase = []
     for (let player_id in playerMap) {
         let player = playerMap[player_id]
         let loseCount = player.gameCount - player.win
-        player.activity = Object.keys(player.gameIdMap).length
-        player.beatCount = Object.keys(player.beatPlayerMap).length
-        if (player.champion > 1) {
+        player.activity = countMap(player.gameIdMap)
+        player.beatCount = countMap(player.beatPlayerMap)
+        let notOneRound = (player.win / player.activity) > 2
+        if (notOneRound && player.activity > 1) {
             // if (player.champion && loseCount < 10) {
             // console.log('lose 10', player);
-            playerArrAnimal.push(player)
+            if (player.master > 0)
+                playerArrRankBase.push(player)
         }
     }
     // playerArrAnimal.sort(ascendingProp('beatCount'))
-    playerArrAnimal.sort(descendingProp('beatCount'))
-    console.log('lose 10', playerArrAnimal);
+    // playerArrRankBase.sort(descendingProp('beatCount'))
+    // console.log('rank base', playerArrRankBase.length, playerArrRankBase);
+    // circlePlayer("4", playerMap, 3)
 
-    circlePlayer("4", playerMap, 3)
 }
+let genBeatRaito = (player, playerMap = null) => {
+    if (playerMap)
+        player = playerMap[player.player_id]
+    let sum = 0
+    for (let pid in player.beatPlayerMap) {
+        sum += player.beatPlayerMap[pid]
+        if (player.losePlayerMap[pid]) {
+            // console.log(player.name, 'lose ', playerMap[pid].name, player.losePlayerMap[pid]);
+            // console.log(player.name, 'win ', playerMap[pid].name, player.beatPlayerMap[pid]);
+            sum += player.losePlayerMap[pid]
+        }
+    }
+    return sum
+}
+
+function sumGame(playerMap, rankArr) {
+
+    let playerArrRankBase = []
+    for (let player_id in playerMap) {
+        let player = playerMap[player_id]
+        let loseCount = player.gameCount - player.win
+        player.activity = countMap(player.gameIdMap)
+        player.beatCount = countMap(player.beatPlayerMap)
+        player.beatRaito = genBeatRaito(player)
+        let notOneRound = player.gameCount > 2
+        if (notOneRound) {
+            // if (player.master > 0)
+            playerArrRankBase.push(player)
+        }
+    }
+    // if (!rankArr) {
+    let sortA = playerArrRankBase.sort(descendingProp('beatRaito'))
+        // }
+    console.log('rank base not one Round', sortA.length, sortA);
+    return sortA
+        // if (playerArrRankBase.length)
+        //     return rankIn(playerArrRankBase, playerMap)
+}
+
+function mergeRank(rankArrOld, rankArrNew, playerMapSum) {
+    let rMerge = rankArrOld.concat()
+        // let genLink = (player, header, tail) => {
+        //     return { header: header, player: player, tail: tail }
+        // }
+    let findRankIn = (player, rankInArr) => {
+        for (let i = 0; i < rankInArr.length; i++) {
+            let p = rankInArr[i];
+            if (p.player_id == player.player_id) {
+                return i;
+            }
+        }
+        return -1
+    }
+
+    let rankByBeatRaito = (player, playerMapSum) => {
+        let pInsertBeatRaito = genBeatRaito(player, playerMapSum)
+        let isInsert = false
+        for (let i = 0; i < rMerge.length; i++) {
+            let p = rMerge[i];
+            if (p.player_id != player.player_id) {
+                if (genBeatRaito(p, playerMapSum) < pInsertBeatRaito) {
+                    // console.log('insert ', rMerge[i]);
+                    rMerge.splice(i, 0, player)
+                        // console.log('insert ', rMerge[i], rMerge[i + 1]);
+                    isInsert = true
+                    break;
+                }
+            }
+        }
+        if (!isInsert)
+            rMerge.push(player)
+    }
+
+    for (let i = 0; i < rankArrNew.length; i++) {
+        let pNew = rankArrNew[i];
+        for (let i = 0; i < rankArrOld.length; i++) {
+            let pOld = rankArrOld[i];
+            if (pNew.player_id != pOld.player_id) {
+                if (isAwinB(pNew, pOld, playerMapSum)) {
+                    // if()
+                    let oldARank = findRankIn(pNew, rankArrOld)
+                    let oldBRank = findRankIn(pOld, rankArrOld)
+                    if (oldARank < oldBRank) { //
+
+                    } else {
+                        console.log('todo 修正交手排名');
+                        // rankArrOld.splice(oldBRank, 0)
+                    }
+                    console.log('oldRank', pNew.name, oldARank, pOld.name, oldBRank);
+                } else {
+                    if (findRankIn(pNew, rMerge) < 0) {
+                        console.log('rank by beatRaito', pNew.name);
+                        rankByBeatRaito(pNew, playerMapSum)
+                    }
+                }
+            } else { //relation
+                // for (let pid in pOld.beatPlayerMap) {
+                //     for (let pid2 in pNew.beatPlayerMap) {
+                //         isAwinB(playerMapSum[pid], playerMapSum[pid2], playerMapSum)
+                //     }
+                // }
+                // console.log('relation', pNew.name);
+            }
+        }
+    }
+    console.log('after merge', rMerge);
+    return rMerge
+}
+
+function rankIn(playerArr, playerMap) {
+    let playerArrRankInOrder = playerArr.sort(ascendingProp('beatCount'))
+
+    // playerArr = playerArr.slice(0, 50)
+    console.log('playArr', playerArr[0], playerArrRankInOrder[0]);
+    let rankArr = [playerArrRankInOrder[0]]
+    for (let i = 1; i < playerArr.length; i++) {
+        let p = playerArr[i];
+        let index = rankArr.length
+        for (let j = 0; j < rankArr.length; j++) {
+            let pr = rankArr[j]
+            if (findWinPath(p.player_id, pr.player_id, playerMap, 0)) {
+                if (p.beatPlayerMap[pr.player_id] > pr.beatPlayerMap[p.player_id])
+                    index = rankArr.length - j
+                else
+                    index = rankArr.length - j + 1
+            }
+        }
+        rankArr.splice(index, 0, p)
+    }
+
+    return rankArr
+}
+
 
 function genRanking() {
     let playerMap = {}
-    let genPlayerActivity = (gameId, player, isWin, against, isFinal) => {
+    let playerMapSum = {}
+    let genPlayerActivity = (gameId, player, myScore, opScore, opPlayer, savePlayerMap) => {
+        let isFinal = (myScore == 5 || opScore == 5)
+        let isMaster = (myScore == 3 || opScore == 3)
+        let isRaw = (myScore == 2 || opScore == 2)
+        let isWin = myScore > opScore
+        let wp = 0.5
+        if (isMaster)
+            wp = 0.7
+        else if (isFinal)
+            wp = 1
         if (player.player_id) {
-            if (!playerMap[player.player_id])
-                playerMap[player.player_id] = genPlayer(player)
-            let playerData = playerMap[player.player_id]
+            if (!savePlayerMap[player.player_id])
+                savePlayerMap[player.player_id] = genPlayer(player)
+            let playerData = savePlayerMap[player.player_id]
             playerData.gameCount++;
             playerData.gameIdMap[gameId] = gameId
             if (isWin) {
                 playerData.win++;
-                if (!playerData.beatPlayerMap[against])
-                    playerData.beatPlayerMap[against] = 0
-                playerData.beatPlayerMap[against]++
-                    if (isFinal)
-                        playerData.champion++
+                if (!playerData.beatPlayerMap[opPlayer])
+                    playerData.beatPlayerMap[opPlayer] = 0
+                playerData.beatPlayerMap[opPlayer] += (myScore - opScore) / myScore * wp
+                if (isFinal)
+                    playerData.champion++;
+                if (isMaster)
+                    playerData.master++;
             } else {
-                playerData.losePlayerMap[against] = against
+                if (!playerData.losePlayerMap[opPlayer])
+                    playerData.losePlayerMap[opPlayer] = 0
+                playerData.losePlayerMap[opPlayer] += (myScore - opScore) / opScore * wp
             }
-            if (!playerData.meetPlayerWinRaitoMap[against])
-                playerData.meetPlayerWinRaitoMap[against] = []
-            playerData.meetPlayerWinRaitoMap[against].push(isWin)
+            // if (!playerData.meetPlayerWinRaitoMap[against])
+            //     playerData.meetPlayerWinRaitoMap[against] = []
+            // playerData.meetPlayerWinRaitoMap[against].push(isWin)
             return playerData
         }
         return null
@@ -180,49 +410,60 @@ function genRanking() {
         }
         return g
     }
-    let genBeatRaito = (lPlayer, rPlayer, isLPlayerWin) => {
-        let winCount = Object.keys(lPlayer.beatPlayerMap).length
-        let loseCount = Object.keys(lPlayer.losePlayerMap).length
-        let lBeatRaito = winCount / (winCount + loseCount)
 
-        winCount = Object.keys(rPlayer.beatPlayerMap).length
-        loseCount = Object.keys(rPlayer.losePlayerMap).length
-        let rBeatRaito = winCount / (winCount + loseCount)
-
-        // if (isLPlayerWin) {
-        //     lPlayer.beatRaito =
-        // }
-
-    }
     gameDb.find({ idx: 2017 }, (err, doc) => {
         console.log('game arr', doc[0]);
-
+        let rankArr;
+        let rankArrOld;
         //402 肯帝亚 123 124 2016总决赛
         let passGameIdxArr = [123, 124, 402]
             // for (let i = 0; i < 5; i++) {
+        let sumCount = 0
+        let sumLimit = 2
         for (let i = 0; i < doc[0].gameArr.length; i++) {
+            // for (let i = 0; i < 2; i++) {
             let game = doc[0].gameArr[doc[0].gameArr.length - 1 - i]
             let info = game.info
             if (passGameIdxArr.indexOf(info.id) > -1 || game.gameArr.length == 0) {
 
             } else {
+                playerMap = {}
                 let validGameArr = genValidGameArr(game.gameArr)
                 console.log(info.game_start.split(' ')[0], info.id, info.title, validGameArr.length);
+                // let oneGamePlayerMap = {}
                 for (let validGame of validGameArr) {
                     let lScore = Number(validGame.left.score)
                     let rScore = Number(validGame.right.score)
-                    let isFinal = (lScore == 5 || rScore == 5)
+
                     let lPlayer = validGame.left
                     let rPlayer = validGame.right
-                    let lPlayer2 = genPlayerActivity(info.id, lPlayer, lScore > rScore, rPlayer.player_id, isFinal)
-                    let rPlayer2 = genPlayerActivity(info.id, rPlayer, lScore < rScore, lPlayer.player_id, isFinal)
-                    if (lPlayer2 && rPlayer2)
-                        genBeatRaito(lPlayer2, rPlayer2, lScore > rScore)
+                    let lPlayer2 = genPlayerActivity(info.id, lPlayer, lScore, rScore, rPlayer.player_id, playerMap)
+                    let rPlayer2 = genPlayerActivity(info.id, rPlayer, rScore, lScore, lPlayer.player_id, playerMap)
+
+                    genPlayerActivity(info.id, lPlayer, lScore, rScore, rPlayer.player_id, playerMapSum)
+                    genPlayerActivity(info.id, rPlayer, rScore, lScore, lPlayer.player_id, playerMapSum)
+                        // if (lPlayer2 && rPlayer2)
+                        //     genBeatRaito(lPlayer2, rPlayer2, lScore > rScore)
                 }
+                // break;
+                rankArr = sumGame(playerMap, rankArr)
+                if (!rankArrOld)
+                    rankArrOld = rankArr.concat()
+                else
+                    rankArrOld = mergeRank(rankArrOld, rankArr, playerMapSum)
+
+                //test
+                sumCount++
+                if (sumCount > sumLimit - 1)
+                    break;
             }
         }
+        // for (let r of rankArr) {
+        //     console.log('ranking', r.name);
+        // }
+        // rankArr = sumPlayer(playerMap)
+
         // console.log(playerMap)
-        sumPlayer(playerMap)
     })
 }
 genRanking()
